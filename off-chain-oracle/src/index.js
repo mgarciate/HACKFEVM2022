18799f61e89e856c6ff7c2d59de5bc749bb667ca
mgarciate <mgarciate@gmail.com>
2022-11-20 17:47:55 +0100

    Add off-chain-oracle

---
+++ off-chain-oracle/src/index.js
@@ -0,0 +1,9 @@
+// Source: https://github.com/pedroduartecosta/blockchain-oracle
+
+import startOracle from "./oracle";
+import startConsumer from "./consumer";
+import startClient from "./client";
+
+startOracle();
+startConsumer();
+startClient();
